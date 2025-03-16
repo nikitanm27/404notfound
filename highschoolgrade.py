9b178b2d88e09caed7d0cf273d25764e031780be
Nikita <sajinikita@gmail.com>
2025-03-16 22:14:22 +0530

    create highschoolgrade.py
    Control Flow Code for:
    U.S. high schools typically last for four years, from freshman year to senior year. 🚌💨
    
    First, ask the user to enter their grade as an integer.
    
    Create a four-year high school grade system using an if/elif/else statement:
    
    grade is 9, print 'Freshman'
    grade is 10, print 'Sophomore'
    grade is 11, print 'Junior'
    grade is 12, print 'Senior'
    Everything else is 'TBD'

---
+++ highschoolgrade.py
@@ -0,0 +1,13 @@
+#Ask the user to enter their grade
+grade= int(input('Enter your grade(9-12: '))
+#create a flow for each grade
+if grade ==9:
+  print('Freshman')
+elif grade ==10:
+  print('Sophomore')
+elif grade ==11:
+  print('Junior')
+elif grade ==12:
+  print('Senior')
+else:
+  print('TBD')
